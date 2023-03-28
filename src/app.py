bd32cd3bebda7d6654fd41a5d93ddac0915937c7
AsmirMumin <asmir.muminovic@tenera.io>
2023-03-28 18:56:07 +0200

    optimized api

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
-import io
+import logging
+import threading
+import time
+from functools import lru_cache
 
 import cv2
 import joblib
@@ -11,48 +14,70 @@ from config import Location
 
 location: Location = Location()
 
-# Load the saved model
-with open(location.model, "rb") as f:
-    model = joblib.load(f)
 
-with open(location.scaler, "rb") as f:
-    scaler = joblib.load(f)
+# cache model and scaler to not reload them for every request
+@lru_cache(maxsize=1)
+def load_model_and_scaler():
 
-# Create a FastAPI app
+    with open(location.model, "rb") as f:
+        model = joblib.load(f)
+
+    with open(location.scaler, "rb") as f:
+        scaler = joblib.load(f)
+
+    return model, scaler
+
+
+model, scaler = load_model_and_scaler()
+
 app = FastAPI(title="Fruit Classifier API")
+
+logging.basicConfig(filename="fruit_classifier.log", level=logging.INFO)
+
+fruit_count = {"apple": 0, "banana": 0}
 
 
 class Img(BaseModel):
     img_path: str
 
 
-# Define an API endpoint
 @app.post("/predict", status_code=200)
 async def predict(request: Img):
 
     try:
-        # Load image
         image = cv2.imread(request.img_path, cv2.IMREAD_COLOR)
-        # Preprocess the image
         image = cv2.resize(image, (100, 100))
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         image = np.array(image)
 
-        # Flatten the image
         image_flat = image.flatten()
 
-        # Scale the flattened image data
         image_scaled = scaler.transform([image_flat])
 
-        # Make predictions on the image data
         prediction = model.predict(image_scaled)
 
-        # Return the predictions as a JSON response
-        return {"predicted fruit": prediction[0]}
+        logging.info(f"Predicted Fruit: {prediction[0]}")
+        fruit_count[prediction[0]] += 1
+
+        return {"Predicted Fruit": prediction[0]}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
+def log_fruit_count():
+    while True:
+        # Log the fruit count
+        logging.info(f"Fruit count: {fruit_count}")
+        # Reset the fruit count
+        fruit_count["apple"] = 0
+        fruit_count["banana"] = 0
+        # Wait for 30 seconds before logging again
+        time.sleep(30)
+
+
 if __name__ == "__main__":
+
+    thread = threading.Thread(target=log_fruit_count)
+    thread.start()
 
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
